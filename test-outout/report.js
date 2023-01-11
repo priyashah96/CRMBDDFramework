e8e59c3dfe0a7ab92e4771b041d307c7da12613b
priyashah <priyashah@LAPTOP-M56SSIE9.ht.home>
2023-01-10 22:02:32 -0500

    updated

---
+++ test-outout/report.js
@@ -0,0 +1,254 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/QA/Selenium_Workspace/FreeCRMBDDFramework/src/main/java/Features/taggedhooks.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Test Tagged Hooks",
+  "description": "",
+  "id": "test-tagged-hooks",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 709999,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 353200,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 1218400,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 244300,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "This is First test",
+  "description": "",
+  "id": "test-tagged-hooks;this-is-first-test",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@First"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "this is the first step",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "this is the second step",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "this is the third step",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TaggedHooksStepDefinition.this_is_the_first_step()"
+});fformatter.result({
+  "duration": 774541599,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TaggedHooksStepDefinition.this_is_the_second_step()"
+});
+formatter.result({
+  "duration": 226600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TaggedHooksStepDefinition.this_is_the_third_step()"
+});
+formatter.result({
+  "duration": 2509101,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 274401,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 105499,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 175300,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 251199,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 297700,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 216301,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 270800,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 208400,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 10,
+  "name": "This is Second test",
+  "description": "",
+  "id": "test-tagged-hooks;this-is-second-test",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 9,
+      "name": "@Second"
+    }
+  ]
+});
+formatter.step({
+  "line": 11,
+  "name": "this is the first step",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 12,
+  "name": "this is the second step",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 13,
+  "name": "this is the third step",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TaggedHooksStepDefinition.this_is_the_first_step()"
+});
+formatter.result({
+  "duration": 200900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TaggedHooksStepDefinition.this_is_the_second_step()"
+});
+formatter.result({
+  "duration": 74200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TaggedHooksStepDefinition.this_is_the_third_step()"
+});
+formatter.result({
+  "duration": 169501,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 185000,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 175200,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 151400,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 51601,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 681700,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 257401,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 281901,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 215300,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 16,
+  "name": "This is Third test",
+  "description": "",
+  "id": "test-tagged-hooks;this-is-third-test",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 15,
+      "name": "@Third"
+    }
+  ]
+});
+formatter.step({
+  "line": 17,
+  "name": "this is the first step",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 18,
+  "name": "this is the second step",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 19,
+  "name": "this is the third step",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TaggedHooksStepDefinition.this_is_the_first_step()"
+});
+formatter.result({
+  "duration": 155500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TaggedHooksStepDefinition.this_is_the_second_step()"
+});
+formatter.result({
+  "duration": 62200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TaggedHooksStepDefinition.this_is_the_third_step()"
+});
+formatter.result({
+  "duration": 59001,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 57499,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 217800,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 208100,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 161301,
+  "status": "passed"
+});
+}););

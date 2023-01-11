2939d714f563c0b4ce2632e2a2366edfb62e868d
priyashah <priyashah@LAPTOP-M56SSIE9.ht.home>
2023-01-10 21:56:19 -0500

    Updated by priya shah

--- src/main/java/Features/login.feature
+++ src/main/java/Features/login.feature
@@ -16,13 +16,13 @@ Scenario Outline: Free CRM Login Test Scenario
 
 Given user is already on Login Page
 When title of login page is Free CRM
-Then user enters "<username>" and "<password>"
+Then user enters "<email>" and "<password>"
 Then user clicks on login button
 Then user is on home page
 Then Close the browser
 
 
 Examples:
-	| username | password |
-	| naveenk  | test@123 |
-	|  tom     | test456  | 
+	| email										  | password 	|
+	| priya.shah4996@gmail.com  | Hapy@2005 |
+	
--- src/main/java/StepDefinitions/LoginStepDefinition.java
+++ src/main/java/StepDefinitions/LoginStepDefinition.java
@@ -1,11 +1,13 @@
 package StepDefinitions;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByClassName;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import cucumber.api.java.en.Given;
@@ -20,9 +22,9 @@ public class LoginStepDefinition{
 	
 	 @Given("^user is already on Login Page$")
 	 public void user_already_on_login_page(){
-	 System.setProperty("webdriver.chrome.driver","/Users/naveenkhunteta/Downloads/chromedriver");
-	 driver = new ChromeDriver();
-	 driver.get("https://www.freecrm.com/index.html");
+	 System.setProperty("webdriver.gecko.driver", "C:\\QA\\SeleniumJars\\geckodriver.exe");
+	 driver = new FirefoxDriver();
+	 driver.get("https://ui.freecrm.com/");
 	 }
 	
 	
@@ -30,23 +32,27 @@ public class LoginStepDefinition{
 	 public void title_of_login_page_is_free_CRM(){
 	 String title = driver.getTitle();
 	 System.out.println(title);
-	 Assert.assertEquals("#1 Free CRM for Any Business: Online Customer Relationship Software", title);
+	 Assert.assertEquals("Cogmento CRM", title);
 	 }
+	 
+	 
+				 
+	
 	
 	 //Reg Exp:
 	 //1. \"([^\"]*)\"
 	 //2. \"(.*)\"
 	
 	 @Then("^user enters \"(.*)\" and \"(.*)\"$")
-	 public void user_enters_username_and_password(String username, String password){
-	 driver.findElement(By.name("username")).sendKeys(username);
+	 public void user_enters_username_and_password(String email, String password){
+	 driver.findElement(By.name("email")).sendKeys(email);
 	 driver.findElement(By.name("password")).sendKeys(password);
 	 }
 	
 	 @Then("^user clicks on login button$")
 	 public void user_clicks_on_login_button() {
 	 WebElement loginBtn =
-	 driver.findElement(By.xpath("//input[@type='submit']"));
+	 driver.findElement(By.xpath("//div[@class='ui fluid large blue submit button']"));
 	 JavascriptExecutor js = (JavascriptExecutor)driver;
 	 js.executeScript("arguments[0].click();", loginBtn);
 	 }
@@ -56,25 +62,25 @@ public class LoginStepDefinition{
 	 public void user_is_on_hopme_page(){
 	 String title = driver.getTitle();
 	 System.out.println("Home Page title ::"+ title);
-	 Assert.assertEquals("CRMPRO", title);
+	 Assert.assertEquals("Cogmento CRM", title);
 	 }
 	 
 	 @Then("^user moves to new contact page$")
 	 public void user_moves_to_new_contact_page() {
 		driver.switchTo().frame("mainpanel");
 		Actions action = new Actions(driver);
-		action.moveToElement(driver.findElement(By.xpath("//a[contains(text(),'Contacts')]"))).build().perform();
-		driver.findElement(By.xpath("//a[contains(text(),'New Contact')]")).click();
+		action.moveToElement(driver.findElement(By.xpath("//i[contains(text(),'users icon')]"))).build().perform();
+		driver.findElement(By.xpath("//a[contains(text(),'/contacts/new')]")).click();
 		
 		}
 	 
 	 
 	 @Then("^user enters contact details \"(.*)\" and \"(.*)\" and \"(.*)\"$")
-	 public void user_enters_contacts_details(String firstname, String lastname, String position){
-		 driver.findElement(By.id("first_name")).sendKeys(firstname);
-		 driver.findElement(By.id("surname")).sendKeys(lastname);
-		 driver.findElement(By.id("company_position")).sendKeys(position);
-		 driver.findElement(By.xpath("//input[@type='submit' and @value='Save']")).click();
+	 public void user_enters_contacts_details(String firstname, String lastname, String company){
+		 driver.findElement(By.name("first_name")).sendKeys(firstname);
+		 driver.findElement(By.name("last_name")).sendKeys(lastname);
+		 driver.findElement(By.className("search")).sendKeys(company);
+		 driver.findElement(By.xpath("//button[@class='ui linkedin button']")).click();
 	 }
 	 
 
